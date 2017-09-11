27dd8d0117a2c420d30a656695894a4e14420a8d
Cuddlemuffin007 <brennonkrogers@gmail.com>
2017-09-11 15:09:16 +0900

    add any fuction and new project aliases

--- aliases.sh
+++ aliases.sh
@@ -19,7 +19,15 @@ alias retrip_postgres_ctl='pg_ctl -D /usr/local/var/postgres/retrip_localhost -l
 alias retrip_start='retrip_postgres_ctl start'
 alias retrip_stop='retrip_postgres_ctl stop'
 alias retrip_status='retrip_postgres_ctl status'
+# sake db service
+alias sake_postgres_ctl='pg_ctl -D /usr/local/var/postgres/sake_localhost -l /usr/local/var/postgres/server.log'
+alias sake_start='sake_postgres_ctl start'
+alias sake_stop='sake_postgres_ctl stop'
+alias sake_status='sake_postgres_ctl status'
+# redis
+alias redis_start='redis-server /usr/local/etc/redis.conf &'
+alias redis_stop='redis-cli -h 127.0.0.1 -p 6379 shutdown'
 # kill dev-retrip tmux session and stop db
-alias kill-retrip='tmuxk dev-retrip && retrip_stop'
+alias kill_retrip='tmuxk dev-retrip && retrip_stop && redis_stop'
 # show unapplied migrations in a django project
 alias show_unapplied='python manage.py showmigrations --list --settings=$DJANGO_SETTINGS_MODULE | grep -v "[X]"'
--- functions.sh
+++ functions.sh
@@ -9,3 +9,15 @@ weather() {
     curl http://wttr.in/$1 || echo "An error occurred. Did you enter a valid city?"
 }
 # >>2
+
+# display searched process name if process is running
+#--------------------------------------------------------------------
+# Usage: any postgres
+# -------------------------------------------------------------------
+any() {
+    if [[ $# == 0 ]]; then
+        echo "usage: any name"
+    fi
+    ps aux | grep -v "grep" | grep $1 || echo "No running processes."
+}
+# >>2
