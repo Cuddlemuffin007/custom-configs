ea995ea5bee575592c29ebf47bf0e01b7e3f3296
Cuddlemuffin007 <brennonkrogers@gmail.com>
2019-02-07 11:33:35 +0900

    update aliases and add vim plugins for typescript

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,4 @@
+alias vi='vim'
 # remove export GREP_OPTIONS="--color=auto" which is deprecated to alias
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
@@ -13,25 +14,25 @@ alias gd='git diff'
 alias gds='git diff --staged'
 # update gh-pages
 alias ghp='git checkout gh-pages;git merge master;git push origin gh-pages;git checkout master'
-# postgres
-alias postgres_ctl='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log'
-alias pg_start='postgres_ctl start'
-alias pg_stop='postgres_ctl stop'
-alias pg_status='postgres_ctl status'
-# retrip db service
-alias retrip_postgres_ctl='pg_ctl -D /usr/local/var/postgres/retrip_localhost -l /usr/local/var/postgres/server.log'
-alias retrip_start='retrip_postgres_ctl start'
-alias retrip_stop='retrip_postgres_ctl stop'
-alias retrip_status='retrip_postgres_ctl status'
-# sake db service
-alias sake_postgres_ctl='pg_ctl -D /usr/local/var/postgres/sake_localhost -l /usr/local/var/postgres/server.log'
-alias sake_start='sake_postgres_ctl start'
-alias sake_stop='sake_postgres_ctl stop'
-alias sake_status='sake_postgres_ctl status'
-# redis
-alias redis_start='redis-server /usr/local/etc/redis.conf &'
-alias redis_stop='redis-cli -h 127.0.0.1 -p 6379 shutdown'
-# kill dev-retrip tmux session and stop db
-alias kill_retrip='tmuxk dev-retrip && retrip_stop && redis_stop'
+# OSX
+if [ "$(uname)" = "Darwin" ]; then
+    # postgres
+    alias postgres_ctl='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log'
+    alias pg_start='postgres_ctl start'
+    alias pg_stop='postgres_ctl stop'
+    alias pg_status='postgres_ctl status'
+    # redis
+    alias redis_start='redis-server /usr/local/etc/redis.conf &'
+    alias redis_stop='redis-cli -h 127.0.0.1 -p 6379 shutdown'
+# *nix
+else
+    # postgres
+    alias pg_start='sudo service postgresql start'
+    alias pg_stop='sudo service postgresql stop'
+    alias pg_status='sudo service postgresql status'
+    # redis
+    alias redis_start='sudo redis-server /etc/redis/redis.conf &'
+    alias redis_stop='sudo redis-cli -h 127.0.0.1 -p 6379 shutdown'
+fi
 # show unapplied migrations in a django project
 alias show_unapplied='python manage.py showmigrations --list --settings=$DJANGO_SETTINGS_MODULE | grep -v "[X]"'
